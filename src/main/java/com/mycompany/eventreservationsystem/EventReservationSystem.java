89154e3ad9563fe82659d6f4e91ee3e8e04ce57b
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-02 18:47:38 +0000

    first commit created EventReservationSystem class with main method Added checkBookingCode method to validate booking code Implemented password complexity check using regular expressions Added makeReservation method to validate booking code and password Finalized code and added comments for clarity

--- src/main/java/com/mycompany/eventreservationsystem/EventReservationSystem.java
+++ src/main/java/com/mycompany/eventreservationsystem/EventReservationSystem.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.eventreservationsystem;
-
+import java.util.Scanner;
 /**
  *
  * @author RC_Student_lab
@@ -11,6 +11,50 @@ package com.mycompany.eventreservationsystem;
 public class EventReservationSystem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter booking code: ");
+        String bookingCode = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        String message = makeReservation(bookingCode, password);
+        System.out.println(message);
+    }
+
+    public static boolean checkBookingCode(String code) {
+        return !(!code.contains("_") || code.length() > 12);
+    }
+
+    public static boolean checkPasswordComplexity(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+        boolean hasCapitalLetter = false;
+        boolean hasNumber = false;
+        boolean hasSpecialCharacter = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasCapitalLetter = true;
+            } else if (Character.isDigit(c)) {
+                hasNumber = true;
+            } else if (!Character.isLetterOrDigit(c)) {
+                hasSpecialCharacter = true;
+            }
+            if (hasCapitalLetter && hasNumber && hasSpecialCharacter) {
+                break;
+            }
+        }
+        return hasCapitalLetter && hasNumber && hasSpecialCharacter;
+    }
+
+    public static String makeReservation(String bookingCode, String password) {
+        if (!checkBookingCode(bookingCode)) {
+            return "Booking code is incorrectly formatted.";
+        } else if (!checkPasswordComplexity(password)) {
+            return "Password does not meet the complexity requirements.";
+        } else {
+            return "Reservation successfully made!";
+        }
     }
 }
+    }
+
